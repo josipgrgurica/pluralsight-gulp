608a77fa8b5e312ab79d4b5b1dd4f22687eb00ad
jgrgurica <jgrgurica@extensionengine.com>
2015-05-19 16:38:10 +0200

    CSS compilation

--- gulp.config.js
+++ gulp.config.js
@@ -1,14 +1,17 @@
 module.exports = function () {
+    var client = './src/client/';
+
     var config = {
         /**
          * File paths
          */
-
+        temp: './.tmp/',
         // all js to vet
         alljs: [
             './src/**/*.js',
             './*.js'
-        ]
+        ],
+        less: client + 'styles/styles.less'
     };
 
     return config;
