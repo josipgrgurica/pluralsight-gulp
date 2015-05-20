b38b4973c84303a9c16941322887d83b731dd2c2
jgrgurica <jgrgurica@extensionengine.com>
2015-05-20 23:28:35 +0200

    HTML injection

--- gulp.config.js
+++ gulp.config.js
@@ -1,17 +1,46 @@
 module.exports = function () {
     var client = './src/client/';
+    var clientApp = client + 'app/';
+    var temp = './.tmp/';
 
     var config = {
         /**
          * File paths
          */
-        temp: './.tmp/',
+
+        temp: temp,
         // all js to vet
         alljs: [
             './src/**/*.js',
             './*.js'
         ],
-        less: client + 'styles/styles.less'
+        css: temp + 'styles.css',
+        client: client,
+        index: client + 'index.html',
+        js: [
+            clientApp + '**/*.module.js',
+            clientApp + '**/*.js',
+            '!' + clientApp + '**/*.spec.js'
+        ],
+        less: client + 'styles/styles.less',
+
+        /**
+         * Bower and NPM locations
+         */
+        bower: {
+            json: require('./bower.json'),
+            directory: './bower_components',
+            ignorePath: '../..'
+        }
+    };
+
+    config.getWiredepDefaultOptions = function () {
+        var options = {
+            bowerJson: config.bower.json,
+            directory: config.bower.directory,
+            ignorePath: config.bower.ignorePath
+        };
+        return options;
     };
 
     return config;
